352d9af00cc5f26241fd5a828e2f77bec434cd62
Haad Bhutta <112280419+hbhutta@users.noreply.github.com>
2023-04-16 10:55:47 -0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,9 @@
+"""
+Papers:
+    - https://ieeexplore.ieee.org/document/9253125
+1. Create scraping script to get data of professional Go games in SGF format from:
+    - https://homepages.cwi.nl/~aeb/go/games/games/
+    - 
+2. Train model
+3. Implement model in C++
+"""
